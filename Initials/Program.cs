6e4d416e1598dcbeac1ca57f92498aab243c936c
sebudwell@gmail.com <sebudwell@gmail.com>
2019-03-09 14:39:00 -0600

    Code works/fails test

--- Initials/Program.cs
+++ Initials/Program.cs
@@ -6,9 +6,34 @@ namespace Initials
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Enter your full name: ");
+            string nameInitials = Console.ReadLine();
+
+            string[] strSplit = nameInitials.Split();
+            foreach (string res in strSplit)
+            {
+                Console.Write(res.Substring(0, 1));
+            }
+
+            Initials(nameInitials);
+
             Console.ReadLine();
+        }
+
+        public static string Initials(string nameInitials)
+        {
+            
+            nameInitials = nameInitials.ToUpper();
+            string[] strSplit = nameInitials.Split();
+            //foreach (string res in strSplit)
+            //{
+            //    Console.Write(res.Substring(0, 1));
+            //}
+            
+            return nameInitials;
         }
 
         // TODO: Create a method that takes in a full name and return the initials in all capital letters.
     }
 }
+  
